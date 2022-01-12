9f9346f13dd72331ace176db5b0816a856f1bde0
juju2010 <78252802+juju2010@users.noreply.github.com>
2022-01-11 21:00:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,45 @@ timer_check = "";
 drawn_sketch= "";
 answer_holder= "";
 score= 0;
+
+function updateCanvas() {
+    background("white");
+    random_number = Math.floor(Math.random()*quick_draw_data_set.length)+1
+    console.log(quick_draw_data_set[random_number]);
+    document.getElementById("draw_sketch").innerHTML = "sketch to be drawn: " + sketch_name
+}
+
+function setup(){
+    canvas = createCanvas(280,280);
+canvas.center();
+background("white")
+}
+
+function draw(){
+    check_sketch();
+    if (drawn_sketch == sketch_name)
+    {
+        answer_holder = "set";
+        score++
+        document.getElementById("score").innerHTML = "score: " + score;
+    }
+}
+
+function check_sketch(){
+    timer_counter = timer_counter + 1;
+    document.getElementById("timer").innerHTML = "Timer: " + timer_counter;
+    console.log(timer_counter);
+    if (timer_counter > 400)
+    {
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+
+    if (timer_check == "completed" || answer_holder == "set")
+    {
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
+
